80da45c172758ce8f1f99a7121f0387496c99e38
Andrey <easycook54@yandex.ru>
2022-08-03 08:22:34 +0700

    Домашняя работа к лекции 3 Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = arr1.length === arr2.length && arr1.every((item,i) => item === arr2[i]); 
+  //если длины массивов одинаковые и для каждого элемента из массива arr1 на позиции i должно быть равенство с каждым элементом из массива arr2 на аналогичной позиции
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
+  // последовательно перебираем числа массива и возвращаем результат соответсвующий условию
   return resultArr; // array
 }
